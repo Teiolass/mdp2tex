9caf17439790e936c5d2351148e17148608094a0
Alessio Marchetti <alessio.marchetti22@gmail.com>
2019-09-20 18:49:05 +0200

    regex support

--- mdp.py
+++ mdp.py
@@ -1,5 +1,6 @@
 from cfg_parser import parse_cfg 
 from stack import Symstack
+import re
 
 src_path = 'f.mdp'
 cfg_path = 'f.cfg'
@@ -12,20 +13,17 @@ with open(src_path, 'r') as inp:
     for line in inp:
         els = []
         for b in blocks:
-            #TODO: rough here
-            n = line.find(b['mdp'])
-            while n != -1:
-                els.append((n, b))
-                n = line.find(b['mdp'], n+1)
+            x = [(m.start(0), m.end(0), b) for m in re.finditer(b['mdp'], line)]
+        els += x
         els.sort(key=lambda x: x[0])
         nl = ''
         last = 0
-        for n, b in els:
+        for n, e, b in els:
             s = stack.add(b)            
             if n != 0:
                 nl += line[last:n]
             nl += s
-            last = n + len(b['mdp'])
+            last = e
         nl += line[last:]
         txt += nl
 if stack.len != 0:
