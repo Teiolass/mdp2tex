7471544dbc4a9ff559e0b3b7bc0305a9394574ae
Alessio Marchetti <alessio.marchetti22@gmail.com>
2019-09-24 14:20:27 +0200

    Intro read

--- mdp.py
+++ mdp.py
@@ -44,7 +44,20 @@ def simple_read(lines, blocks):
         raise Exception('Error: blocks do not close')
 
 
+def intro_read(lines):
+    attributes = {}
+    for line in lines:
+        if 9*'-' in line:
+            return attributes
+        att = re.findall(r'^.*(?=:)', line)
+        val = re.findall(r'(?<=").*(?=")', line)
+        if len(att) != 0 and len(val) != 0:
+            attributes[att[0]] = val[0]
+
+
 blocks = parse_cfg(cfg_path)
 with open(src_path, 'r') as inp:
+    attributes = intro_read(inp)
+    print(attributes)
     for nl in simple_read(inp, blocks):
         print(nl, end='')
