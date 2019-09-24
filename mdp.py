baf6300dd35b0c07e2de2062414ad022946f39d9
Alessio Marchetti <alessio.marchetti22@gmail.com>
2019-09-24 13:23:03 +0200

    simple read is a generator now

--- mdp.py
+++ mdp.py
@@ -1,24 +1,22 @@
-from cfg_parser import parse_cfg 
+from cfg_parser import parse_cfg
 from stack import Symstack
 import re
 
 src_path = 'f.mdp'
 cfg_path = 'f.cfg'
 
-stack = Symstack()
-txt = ''
 
-blocks = parse_cfg(cfg_path)
-with open(src_path, 'r') as inp:
+def simple_read(lines, blocks):
+    stack = Symstack()
     for line in inp:
         els = []
         for b in blocks:
             if b['mode'] == 'brack':
-                x = [(m.start(0), m.end(0), b) for m in 
+                x = [(m.start(0), m.end(0), b) for m in
                         re.finditer(b['mdp'], line)]
                 els += x
             if b['mode'] == 'line':
-                x = [(m.start(0), m.end(0), b) for m in 
+                x = [(m.start(0), m.end(0), b) for m in
                         re.finditer(b['mdp'], line)]
                 if len(x) != 0:
                     last = len(line) - 1
@@ -30,21 +28,23 @@ with open(src_path, 'r') as inp:
                     els.append((0, len(line)-1, b))
         if re.match(r'\'\'\'$', line):
             els.append((0, len(line)-1, 'ENDBLOCK'))
-                
+
         els.sort(key=lambda x: x[0])
         nl = ''
         last = 0
         for n, e, b in els:
-            s = stack.add(b)            
+            s = stack.add(b)
             if n != 0:
                 nl += line[last:n]
             nl += s
             last = e
         nl += line[last:]
-        txt += nl
-if stack.len != 0:
-    raise Exception('Error: blocks do not close')
+        yield nl
+    if stack.len != 0:
+        raise Exception('Error: blocks do not close')
 
-print(txt)
-            
 
+blocks = parse_cfg(cfg_path)
+with open(src_path, 'r') as inp:
+    for nl in simple_read(inp, blocks):
+        print(nl, end='')
